9a83b599ef4a4fd427ec76a0a0c56575a4db352a
Oliver Arafat <arafato@amazon.de>
2015-10-09 01:27:57 -0700

    deleted

--- es/twitter-ingest/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    consumerKey:         'XXX',
-    consumerSecret:      'XXX',
-    accessToken:         'XXX',
-    accessTokenSecret:  'XXX',
-    streamName: 'social-rt-demo'
-};
